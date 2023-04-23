aae630fe170835343a61e3f39796d9f7d6e234ad
i-martello <martelloignacio@gmai.com>
2023-04-23 02:48:33 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -8,9 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
-app.use(cors({
-  origin: 'http://127.0.0.1:5173'
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/productos', router);
 
