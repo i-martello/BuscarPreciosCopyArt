e22cc143403a3168a583666482ed30e45af9f1b9
i-martello <martelloignacio@gmai.com>
2023-04-23 13:22:21 -0300

    cambios

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -11,13 +11,29 @@ ctrlProduct.getStatic = async (req, res)=> {
 }
 
 ctrlProduct.getAll = async (req, res)=>{
+
+  let skip = 0
+  let limit = 50
+  let documentos = []
+
+  const obtenerDatos = async ()=>{
     let filtro = {}
     if(req.query.search){
       const { search } = req.query
       filtro = {name: { $regex: search, $options: 'i' }}
     }
-    const productos = await productoSchema.find(filtro);
-      res.json({productos});
+    const productos = await productoSchema.find(filtro).skip(skip).limit(limit);
+
+    documentos = documentos.concat(productos);
+    skip += limit
+    if(limit === productos.length){
+      await obtenerDatos();
+    } else {
+      res.json({documentos});
+    }
   }
+  obtenerDatos()
+
+}
 
 module.exports = ctrlProduct;
